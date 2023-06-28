bf7ae5ddd9ea3bfefb46e496f55a1bc5e948e2a8
dannyyuan06 <dannyyuan06@gmail.com>
2023-06-28 17:42:16 +0100

    camel

--- src/app/navigationBar/Navigation.tsx
+++ src/app/navigationBar/Navigation.tsx
@@ -12,25 +12,25 @@ const imageStyle:CSSProperties = {
 }
 
 export function Navigation() {
-    const [currentPage, setCurrentPage] = useState('ADMIN DASHBOARD')
+    const [currentPage, setCurrentPage] = useState('adminDashboard')
 
     useEffect(() => {
-
+        
     }, [currentPage])
 
 
     return (
         <nav className={styles.container}>
             <div className={styles.wrapper}>
-                <button className={styles.logoButton} onClick={() => setCurrentPage("ADMIN DASHBOARD")}>
+                <button className={styles.logoButton} onClick={() => setCurrentPage("adminDashboard")}>
                 <Image src='/images/TRC_Logo_Primary_RGB_Lge.png' width={160} height={160} alt='TRC Logo' style={imageStyle}/>
                 </button>
                 <ul className={styles.ul}>
-                    <NavigationButton currentPage={currentPage} setCurrentPage={setCurrentPage} pageTitle='ADMIN DASHBOARD'/>
-                    <NavigationButton currentPage={currentPage} setCurrentPage={setCurrentPage} pageTitle='ABOUT OUR CLUB'/>
-                    <NavigationButton currentPage={currentPage} setCurrentPage={setCurrentPage} pageTitle='MEMBERS'/>
-                    <NavigationButton currentPage={currentPage} setCurrentPage={setCurrentPage} pageTitle='CURRENTLY READING'/>
-                    <NavigationButton currentPage={currentPage} setCurrentPage={setCurrentPage} pageTitle='MEETINGS'/>
+                    <NavigationButton currentPage={currentPage} setCurrentPage={setCurrentPage} pageTitle='adminDashboard'/>
+                    <NavigationButton currentPage={currentPage} setCurrentPage={setCurrentPage} pageTitle='aboutOurClub'/>
+                    <NavigationButton currentPage={currentPage} setCurrentPage={setCurrentPage} pageTitle='members'/>
+                    <NavigationButton currentPage={currentPage} setCurrentPage={setCurrentPage} pageTitle='currentlyReading'/>
+                    <NavigationButton currentPage={currentPage} setCurrentPage={setCurrentPage} pageTitle='meetings'/>
                 </ul>
             </div>
         </nav>
--- src/app/navigationBar/NavigationButton.tsx
+++ src/app/navigationBar/NavigationButton.tsx
@@ -1,5 +1,6 @@
 import { Dispatch, SetStateAction } from 'react'
 import styles from './navigationButton.module.css'
+import { camelToTitle } from './camelAndTitle.tsx'
 type propsType = {
     currentPage: string,
     setCurrentPage: Dispatch<SetStateAction<string>>,
@@ -16,20 +17,11 @@ export function NavigationButton({currentPage, setCurrentPage, pageTitle}: props
             <div className={styles.marker} style={{backgroundColor: 'var(--theme-blue)'}}>
             </div>}
             <button onClick={clickHandler}>
-                {pageTitle}
+                {camelToTitle(pageTitle)}
             </button>
             {!isActive && 
             <div className={styles.marker}>
             </div>}
         </div>
     )
-}
-
-function titleToCamel(title: string) {
-    const words = title.split(' ')
-    const lowerWords = words.map(word => word.toLowerCase())
-    const withCapital = lowerWords.map(word => word.charAt(0).toUpperCase() + word.slice(1))
-    const joinedWord = withCapital.join("")
-    const finalWord = joinedWord.charAt(0).toLowerCase() + joinedWord.slice(1)
-    return finalWord
 }
