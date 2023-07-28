a10a0a3e093f2abcd55595af4795b87e7b7219f7
Danny Yuan <dannyyuan06@icloud.com>
2023-07-28 14:24:31 +0100

    fixed vercel

--- client/src/app/api/bookDatabase/addBook/route.ts
+++ client/src/app/api/bookDatabase/addBook/route.ts
@@ -6,7 +6,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
     const body:BookType = await req.json()
 
-    const book:BookPrismaType = {
+    const book:any = {
         bookid: body.id,
         title: body.volumeInfo.title,
         pageCount: body.volumeInfo.pageCount,
