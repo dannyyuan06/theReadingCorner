116026f837ec9ed90810e93f0cfa7e6a71017fd6
dannyyuan06 <dannyyuan06@gmail.com>
2023-09-08 16:14:54 +0100

    comments

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -5,13 +5,16 @@ import styles from './layout.module.css'
 import { ReduxProvider } from '@/redux/Provider'
 import { SessionProviders } from './components/SessionProviders'
 
+// For google fonts.
 const inter = Inter({ subsets: ['latin'] })
 
+// Page metadata which could be used for web crawlers like google
 export const metadata = {
   title: 'TRC',
   description: 'The Reading Corner',
 }
 
+// Where the web app root function starts.
 export default function RootLayout({
   children,
 }: {
@@ -23,11 +26,14 @@ export default function RootLayout({
         <link rel="stylesheet" href="https://use.typekit.net/gzh2kwv.css"/>
       </head>
       <body className={inter.className}>
+        {/* For authentication */}
         <SessionProviders>
+          {/* For storing local data */}
           <ReduxProvider>
             <Navigation/>
             <div className={styles.bodyWrapper}>
               <div className={styles.bodyContainer}>
+                {/* Where the pages will be */}
                 {children}
               </div>
             </div>
