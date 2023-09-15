cf3edcef55004ebdc207037fce5d3092ab0238ca
dannyyuan06 <dannyyuan06@gmail.com>
2023-09-15 10:13:22 +0100

    doc

--- client/src/app/currentlyReading/page.tsx
+++ client/src/app/currentlyReading/page.tsx
@@ -4,12 +4,14 @@ import { CurrentlyReadingBook } from "./CurrentlyReadingBook";
 import styles from './page.module.css'
 import { CurrentlyReading } from "@/models/CurrentlyReading";
 
+// Each page is usually cached but this forces it to fetch new
+// data each time
 export const dynamic = 'force-dynamic'
 export const fetchCache = 'force-no-store';
 
 export default async function currentlyReading() {
     const books = await getCurrentlyReadingBooks()
-
+    // If there are no currently reading books in database
     if (books.length === 0) {
         return (
             <div>
@@ -18,19 +20,27 @@ export default async function currentlyReading() {
             </div>
         )
     }
+    // split the books into previous and current books
     const [currentlyReadingBook, ...theRest] = books
     return(
         <div>
             <PageHeader>CURRENTLY READING</PageHeader>
             <CurrentlyReadingBook currentlyReading={currentlyReadingBook}/>
-            {theRest.length !== 0 && <h2 className={styles.previousBooks}>PREVIOUS BOOKS</h2>}
+            {theRest.length !== 0 &&
+                <h2 className={styles.previousBooks}>PREVIOUS BOOKS</h2>
+            }
             {theRest.map(currentlyReading => (
-                <PastReadingBook key={currentlyReading.readid} currentlyReading={currentlyReading}/>
+                <PastReadingBook
+                    key={currentlyReading.readid}
+                    currentlyReading={currentlyReading}
+                />
             ))}
         </div>
     )
 }
 
+// When rendered on server side, get the books from the database.
+// No need to call a lambda function as it itself is a serverless function
 async function getCurrentlyReadingBooks() {
     const [books, err] = await CurrentlyReading.getCurrentlyReadingBooks()
     return books ?? []
--- client/src/app/navigationBar/camelAndTitle.ts
+++ client/src/app/navigationBar/camelAndTitle.ts
@@ -1,8 +1,11 @@
+// This function is not used but it might be useful in the future.
 export function titleToCamel(title: string) {
     const words = title.split(' ')
     const lowerWords = words.map(word => word.toLowerCase())
+    // Add a capital letter for every word
     const withCapital = lowerWords.map(word => word.charAt(0).toUpperCase() + word.slice(1))
     const joinedWord = withCapital.join("")
+    // Removed first capital letter
     const finalWord = joinedWord.charAt(0).toLowerCase() + joinedWord.slice(1)
     return finalWord
 }
@@ -10,17 +13,21 @@ export function titleToCamel(title: string) {
 export function camelToTitle(title: string) {
     let words = []
     let buffer = ''
+    // Loop through every character
     for (let i=0;i<title.length;i++) {
         const currentLetter = title[i]
+        // Start a new word every lowercase letter
         if (currentLetter === currentLetter.toLowerCase()) {
             buffer += currentLetter
         }
+        // Else append to the current word
         else {
             words.push(buffer.toUpperCase())
             buffer = ''
             buffer += currentLetter
         }
     }
+    // Display everything in uppercase
     words.push(buffer.toUpperCase())
     const capitals = words.join(' ')
     return capitals
