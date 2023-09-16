84412d27f1c79756d6334495808ac0d0cda34b21
dannyyuan06 <dannyyuan06@gmail.com>
2023-09-16 19:41:21 +0100

    more comments

--- client/src/app/dashboard/BookTile.tsx
+++ client/src/app/dashboard/BookTile.tsx
@@ -1,38 +1,68 @@
-import Image from 'next/image'
-import styles from './BookTile.module.css'
-import tStyles from './tiles.module.css'
-import Link from 'next/link'
-import { Book, CurrentlyReading } from '@prisma/client'
+import Image from "next/image";
+import styles from "./BookTile.module.css";
+import tStyles from "./tiles.module.css";
+import Link from "next/link";
+import { Book, CurrentlyReading } from "@prisma/client";
 
 interface CurrentlyReadingIncludingBook extends CurrentlyReading {
-    book: Book
+  book: Book;
 }
 
-export function BookTile({book}: {book: CurrentlyReadingIncludingBook}) {
-    const displayAverageRating = book.book.averageRating === -1 ? "-" : book.book.averageRating.toFixed(2) ?? ""
-    return (
-        <div className={tStyles.container}>
-            <div className={styles.container}>
-                <Link href={`/books/${book.book.bookid}`} style={{backgroundColor: 'transparent'}}>
-                    <Image alt="book placeholder" style={{objectFit: 'cover'}} src={book.book.bookPicture} width={100} height={155} className={styles.image}/>
-                </Link>
-                <div className={styles.textContainer}>
-                <div className={styles.headerBodySeparator}>
-                        <div className={styles.titles}>
-                            <Link href={`/books/${book.book.bookid}`}><h2 className={styles.bookTitle}>{book.book.title}</h2></Link>
-                            <h3>AUTHOR: {book.book.author}</h3>
-                            <h3>START DATE: <span>{book.dateStarted.toDateString().split(" ").slice(1).join(" ")}</span></h3>
-                            <h3>PAGE: {book.pageNumber === -1 ? "" : book.pageNumber}/{book.book.pageCount}</h3>
-                        </div>
-                        <div className={styles.scoreContainer}>
-                            <div className={styles.scoreWrapper}>
-                                <h3>SCORE</h3>
-                                <div className={styles.displayScore}>{displayAverageRating}</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+export function BookTile({ book }: { book: CurrentlyReadingIncludingBook }) {
+  // If the database stores -1 as rating it means it doesn't have any ratings yet.
+  const displayAverageRating =
+    book.book.averageRating === -1
+      ? "-"
+      : book.book.averageRating.toFixed(2) ?? "";
+  return (
+    <div className={tStyles.container}>
+      <div className={styles.container}>
+        <Link
+          href={`/books/${book.book.bookid}`}
+          style={{ backgroundColor: "transparent" }}
+        >
+          <Image
+            alt="book placeholder"
+            style={{ objectFit: "cover" }}
+            src={book.book.bookPicture}
+            width={100}
+            height={155}
+            className={styles.image}
+          />
+        </Link>
+        <div className={styles.textContainer}>
+          <div className={styles.headerBodySeparator}>
+            <div className={styles.titles}>
+              <Link href={`/books/${book.book.bookid}`}>
+                <h2 className={styles.bookTitle}>{book.book.title}</h2>
+              </Link>
+              <h3>AUTHOR: {book.book.author}</h3>
+              <h3>
+                START DATE:{" "}
+                <span>
+                  {book.dateStarted
+                    .toDateString()
+                    .split(" ")
+                    .slice(1)
+                    .join(" ")}
+                </span>
+              </h3>
+              <h3>
+                PAGE: {book.pageNumber === -1 ? "" : book.pageNumber}/
+                {book.book.pageCount}
+              </h3>
             </div>
+            <div className={styles.scoreContainer}>
+              <div className={styles.scoreWrapper}>
+                <h3>SCORE</h3>
+                <div className={styles.displayScore}>
+                  {displayAverageRating}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- client/src/app/profile/[userId]/books/[page]/page.tsx
+++ client/src/app/profile/[userId]/books/[page]/page.tsx
@@ -1,50 +1,88 @@
-import { PageHeader } from "@/app/components/PageHeader"
-import User from "@/models/User"
-import styles from './page.module.css'
+import { PageHeader } from "@/app/components/PageHeader";
+import User from "@/models/User";
+import styles from "./page.module.css";
 import { SmallBook } from "../../SmallBook";
 import Link from "next/link";
 
+export default async function UserPage({
+  params,
+}: {
+  params: { page: string; userId: string };
+}) {
+  // Get page number from parameter so can query database
+  const pageNumber = parseInt(params.page);
+  // Server side render this page
+  const [books, err] = await User.paginationBooks(pageNumber, params.userId);
 
-export default async function UserPage({ params }: {params: {page: string, userId: string}}) {
-    const pageNumber = parseInt(params.page)
-
-    const [books, err] = await User.paginationBooks(pageNumber, params.userId);
-
-    if (pageNumber < 1 || !books) return (
-        <div>
-            <PageHeader>
-                INVALID PAGE
-            </PageHeader>
-        </div>
-    )
+  // Validation of page number in case of invalid number
+  if (pageNumber < 1 || !books)
     return (
-        <div>
-            <PageHeader>
-                {params.userId}&apos;S BOOKS
-            </PageHeader>
-            <Buttons username={params.userId} page={pageNumber} length={books.length}/>
-            <div className={styles.booksContainer}>
-                {books.map((userbook) => (
-                    <SmallBook key={userbook.bookid} userbook={userbook}/>
-                ))}
-            </div>
-            <Buttons username={params.userId} page={pageNumber} length={books.length}/>
-        </div>
-    )
+      <div>
+        <PageHeader>INVALID PAGE</PageHeader>
+      </div>
+    );
+  return (
+    <div>
+      <PageHeader>{params.userId}&apos;S BOOKS</PageHeader>
+      <Buttons
+        username={params.userId}
+        page={pageNumber}
+        length={books.length}
+      />
+      <div className={styles.booksContainer}>
+        {books.map((userbook) => (
+          <SmallBook key={userbook.bookid} userbook={userbook} />
+        ))}
+      </div>
+      <Buttons
+        username={params.userId}
+        page={pageNumber}
+        length={books.length}
+      />
+    </div>
+  );
 }
 
-function Buttons({username, page, length}: {username:string, page: number, length:number}) {
-
-    const isLeftAv = page > 1
-    const isRightAv = length === 40
-
-    return (
-        <div className={styles.buttonContainer}>
-            <Link href={`/profile/${username}`} className={styles.link + " " + styles.back}>BACK TO PROFILE</Link>
-            <div className={styles.pageButtonContainer}>
-                {isLeftAv && <Link href={`/profile/${username}/books/${page+1}`} className={styles.link + " " + styles.prev}>PREVIOUS PAGE</Link>}
-                {isRightAv && <Link href={`/profile/${username}/books/${page-1}`} className={styles.link + " " + styles.next}>NEXT PAGE</Link>}
-            </div>
-        </div>
-    )
+// Top and bottom buttons
+function Buttons({
+  username,
+  page,
+  length,
+}: {
+  username: string;
+  page: number;
+  length: number;
+}) {
+  // Work out if you can go back or forwards through the books
+  const isLeftAv = page > 1;
+  const isRightAv = length === 40;
+  
+  return (
+    <div className={styles.buttonContainer}>
+      <Link
+        href={`/profile/${username}`}
+        className={styles.link + " " + styles.back}
+      >
+        BACK TO PROFILE
+      </Link>
+      <div className={styles.pageButtonContainer}>
+        {isLeftAv && (
+          <Link
+            href={`/profile/${username}/books/${page + 1}`}
+            className={styles.link + " " + styles.prev}
+          >
+            PREVIOUS PAGE
+          </Link>
+        )}
+        {isRightAv && (
+          <Link
+            href={`/profile/${username}/books/${page - 1}`}
+            className={styles.link + " " + styles.next}
+          >
+            NEXT PAGE
+          </Link>
+        )}
+      </div>
+    </div>
+  );
 }
