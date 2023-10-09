b5771e5b9a6d861a7aaeb723366ea483c747cd61
Danny Yuan <dannyyuan06@icloud.com>
2023-10-09 19:45:04 +0100

    Completed tests

--- client/src/app/bulletinBoard/WithoutBookMessage.test.tsx
+++ client/src/app/bulletinBoard/WithoutBookMessage.test.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
-import { WithoutBookMessage } from './WithoutBookMessage';
-import { booksRelationshipType, getMessagesType } from '@/models/BulletinBoard';
-import { ProfileFriendType } from '@/models/User';
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { WithoutBookMessage } from "./WithoutBookMessage";
+import { booksRelationshipType, getMessagesType } from "@/models/BulletinBoard";
+import { ProfileFriendType } from "@/models/User";
 import "@testing-library/jest-dom";
+import { useSession } from "next-auth/react";
+import userEvent from "@testing-library/user-event";
 
 // Mock the next-auth/react module for authentication testing
-jest.mock('next-auth/react', () => ({
-  useSession: () => ({ data: { accessLevel: 3 } }), // Mock authentication data for an administrator
+jest.mock("next-auth/react", () => ({
+  useSession: jest.fn().mockReturnValue({ data: { accessLevel: 3 } }), // Mock authentication data for an administrator
 }));
 
 jest.mock("react-redux", () => ({
-  useDispatch: jest.fn()
-}))
+  useDispatch: jest.fn(),
+}));
 
 const mockBooks: booksRelationshipType[] = [
   {
@@ -23,11 +25,11 @@ const mockBooks: booksRelationshipType[] = [
       bookid: "ARandomString",
       bookPicture: "/images/book-placeholder.png",
       title: "A Random string",
-      author: "A Random string",
-      description: "A Random string",
+      author: "A Random author",
+      description: "A Random description",
       pageCount: 1234,
       averageRating: 4,
-    }
+    },
   },
   {
     messageid: 2,
@@ -37,23 +39,23 @@ const mockBooks: booksRelationshipType[] = [
       bookid: "ARandomString2",
       bookPicture: "/images/book-placeholder.png",
       title: "A Random string 2",
-      author: "A Random string 2",
-      description: "A Random string 2",
+      author: "A Random author 2",
+      description: "A Random description 2",
       pageCount: 1234,
       averageRating: 4,
-    }
-  }
-]
+    },
+  },
+];
 
-const mockUser:ProfileFriendType = {
+const mockUser: ProfileFriendType = {
   accessLevel: 1,
   firstName: "John",
   lastName: "Doe",
   lastOnline: new Date(),
   joinDate: new Date(),
   profilePicture: "/images/profile-picture-placeholder.png",
-  username: "JohnDoe"
-}
+  username: "JohnDoe",
+};
 
 const mockMessage: getMessagesType = {
   messageid: 1,
@@ -62,14 +64,54 @@ const mockMessage: getMessagesType = {
   username: "JulianDoe",
   user: mockUser,
   books: mockBooks,
-  reported: false
-}
+  reported: false,
+};
 
-describe('WithoutBookMessage', () => {
-  it('renders message details and buttons correctly', () => {
+describe("WithoutBookMessage", () => {
+  it("renders message details and buttons correctly", () => {
     render(<WithoutBookMessage message={mockMessage} />);
-    
-    expect(screen.getByTestId('without-book-message')).toBeInTheDocument();
-    expect(screen.getByText('This is my first message.')).toBeInTheDocument();
+
+    expect(screen.getByTestId("without-book-message")).toBeInTheDocument();
+    expect(screen.getByText("This is my first message.")).toBeInTheDocument();
+
+    expect(screen.getByText("JohnDoe")).toBeInTheDocument();
+    expect(screen.getByText("A Random string")).toBeInTheDocument();
+  });
+  it("should not render a Delete or Un-report button for a user with an access level of less than 3", async () => {
+    (useSession as any).mockReturnValueOnce({ data: { accessLevel: 1 } })
+    const user = userEvent.setup();
+
+    render(<WithoutBookMessage message={mockMessage} />);
+
+    const moreButtonElement = screen.getByTestId("more-button");
+    await user.click(moreButtonElement);
+
+    const deleteButton = screen.queryByText("Delete");
+    expect(deleteButton).not.toBeInTheDocument();
+
+    const unReportButton = screen.queryByText("Un-report");
+    expect(unReportButton).not.toBeInTheDocument();
+  });
+  it("should render a Delete button for a user with an access level of equal to 3", async () => {
+    const user = userEvent.setup();
+
+    render(<WithoutBookMessage message={mockMessage} />);
+
+    const moreButtonElement = screen.getByTestId("more-button");
+    await user.click(moreButtonElement);
+
+    const deleteButton = screen.getByText("Delete");
+    expect(deleteButton).toBeInTheDocument();
+  });
+  it("should render a Un-report button for a user with an access level of equal to 3 if message is reported", async () => {
+    const user = userEvent.setup();
+
+    render(<WithoutBookMessage message={{...mockMessage, reported: true}} />);
+
+    const moreButtonElement = screen.getByTestId("more-button");
+    await user.click(moreButtonElement);
+
+    const deleteButton = screen.getByText("Un-report");
+    expect(deleteButton).toBeInTheDocument();
   });
 });
