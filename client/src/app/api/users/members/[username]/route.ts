b964ed5d644252f861d7233728daea73cd0f729d
Danny Yuan <dannyyuan06@icloud.com>
2023-10-18 16:34:10 +0100

    Fixed finding algo

--- client/src/app/api/users/members/[username]/route.ts
+++ client/src/app/api/users/members/[username]/route.ts
@@ -5,7 +5,7 @@ import User from "@/models/User";
 export async function GET(req: NextRequest, {params}: {params: {username: string}}) {
     const [access, res] = await apiMiddleware(req, 3)
     if (!access) return res
-    if (params.username.length < 8) return NextResponse.json({users: []})
+    if (params.username.length < 8) return NextResponse.json([])
     const [users, err] = await User.getMembers(params.username)
     if (!users) return NextResponse.json({err}, {status: 400})
     return NextResponse.json(users)
